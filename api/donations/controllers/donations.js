5e2437599757506844feaa8e5cf1d7421cd794f1
Metkel K <metkelk@gmail.com>
2021-04-28 16:58:41 -0400

    configured routes

--- api/donations/controllers/donations.js
+++ api/donations/controllers/donations.js
@@ -1,8 +1,36 @@
-'use strict';
+"use strict";
+
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async setUpStripeSession(ctx) {
+    const { amount } = ctx.request.body;
+    const configuredAmount = amount * 100;
+
+    const session = await stripe.checkout.sessions.create({
+      success_url: "https://localhost:3000",
+      cancel_url: "https://localhost:3000",
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price_data: {
+            unit_amount: configuredAmount,
+            currency: "cad",
+            product_data: {
+              name: "donation",
+              description: "donation amount",
+            },
+          },
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+    });
+    ctx.send({ id: session.id });
+  },
+};
